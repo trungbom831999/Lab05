b500acb99e17d7fb4cf8dd95e473b9beb3b79344
trungbom831999 <trungbomvip831999@gmail.com>
2021-01-05 16:31:21 +0700

    Lab 05

---
+++ Lab05-OMSApp/src/com/oms/serverapi/cd/ICDApi.java
@@ -0,0 +1,11 @@
+package com.oms.serverapi.cd;
+
+import com.oms.bean.CompactDisc;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public interface ICDApi {
+    ArrayList<CompactDisc> getCds(Map<String, String> queryParams);
+    CompactDisc updateCD(CompactDisc cd);
+}
